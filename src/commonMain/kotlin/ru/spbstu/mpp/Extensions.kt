a3ee17d2d016d417a483ce675331e74662d367c3
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2022-07-13 15:51:36 +0300

    Documenting comments

--- src/commonMain/kotlin/ru/spbstu/mpp/Extensions.kt
+++ src/commonMain/kotlin/ru/spbstu/mpp/Extensions.kt
@@ -1,13 +1,34 @@
 package ru.spbstu.mpp
 
-operator fun BitSet.contains(index: Int): Boolean = index < size() && get(index)
+/**
+ * Adhere to kotlin `in` operator convention
+ *
+ * @param element index to check
+ * @see BitSet.get
+ **/
+operator fun BitSet.contains(element: Int): Boolean = element < size() && get(element)
 
+/**
+ * Check whether `this` bitset has all `other` bitset bits also set
+ * @param index index to check
+ **/
 fun BitSet.containsAll(other: BitSet) = other.andNotcardinality(this) == 0
 
+/**
+ * MutableSet wrapper for BitSet
+ *
+ * @see MutableSet
+ */
 private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
+    /**
+     * size is handled and stored separately from the bitset because calculating it is not free
+     */
     override var size: Int = inner.cardinality()
         private set
 
+    /**
+     * recalculate size based on `inner` contents and assign it
+     */
     private fun recalculateSize() {
         size = inner.cardinality()
     }
@@ -18,6 +39,7 @@ private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
             override fun hasNext(): Boolean = innerIterator.hasNext()
             override fun next(): Int = innerIterator.next()
             override fun remove() {
+                /* we need to provide our own remove() because of size */
                 innerIterator.remove()
                 recalculateSize()
             }
@@ -42,6 +64,9 @@ private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
         return true
     }
 
+    /*
+    * Optimized set union: if `elements` is also a bitset, use bitset or operation
+    * */
     override fun addAll(elements: Collection<Int>): Boolean {
         when (elements) {
             is BitSetAsSet -> inner.or(elements.inner)
@@ -52,6 +77,9 @@ private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
         return oldSize != size
     }
 
+    /*
+    * Optimized set inclusion: if `elements` is also a bitset, use bitset containsAll operation
+    * */
     override fun containsAll(elements: Collection<Int>): Boolean {
         return when (elements) {
             is BitSetAsSet -> inner.containsAll(elements.inner)
@@ -59,6 +87,9 @@ private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
         }
     }
 
+    /*
+    * Optimized set difference: if `elements` is also a bitset, use bitset andNot operation
+    * */
     override fun removeAll(elements: Collection<Int>): Boolean {
         when (elements) {
             is BitSetAsSet -> inner.andNot(elements.inner)
@@ -69,6 +100,9 @@ private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
         return oldSize != size
     }
 
+    /*
+    * Optimized set intersection: if `elements` is also a bitset, use bitset and operation
+    * */
     override fun retainAll(elements: Collection<Int>): Boolean {
         when (elements) {
             is BitSetAsSet -> inner.and(elements.inner)
@@ -90,5 +124,18 @@ private class BitSetAsSet(val inner: BitSet): AbstractMutableSet<Int>() {
     }
 }
 
+/**
+ * Wrap this BitSet as a MutableSet
+ *
+ * Any changes to the resulting set will change the contents of BitSet
+ *
+ * It is unsafe to change the contents of this BitSet after it has been wrapped
+ */
 fun BitSet.asMutableSet(): MutableSet<Int> = BitSetAsSet(this)
+
+/**
+ * Wrap this BitSet as a Set
+ *
+ * It is unsafe to change the contents of this BitSet after it has been wrapped
+ */
 fun BitSet.asSet(): Set<Int> = asMutableSet()
